9bf19c2d79d62e868c74136e3add0f974973344c
Artem653 <gromboom1325@gmail.com>
2025-10-01 14:45:12 +0300

    skip/take

--- server.js
+++ server.js
@@ -7,44 +7,65 @@ const app = express();
 const PORT = 8001;
 
 const productsPath = path.join(__dirname, "posts.json");
-const posts = JSON.parse(fs.readFileSync(productsPath, 'UTF-8'))
+const posts = JSON.parse(fs.readFileSync(productsPath, "UTF-8"));
 
-  function getDate() {
-    return moment().format("YYYY/MM/DD HH:mm:ss");
-  }
+function getDate() {
+  return moment().format("YYYY/MM/DD HH:mm:ss");
+}
 
-  app.get("/timestamp", (req, res) => {
-    res.json({ timestamp: getDate() });
-  });
-
+app.get("/timestamp", (req, res) => {
+  res.json({ timestamp: getDate() });
+});
 
 app.get("/posts", (req, res) => {
-  res.json(posts);
+  let { skip, take } = req.query;
+
+  if (skip !== undefined) {
+    skip = Number(skip);
+    if (isNaN(skip) || skip < 0) {
+      res.status(400).json({ error: "Error, take must be a non-negative integer" })
+      return
+    }
+  }
+
+  if (take !== undefined) {
+    take = Number(take);
+    if (isNaN(take) || take < 0) {
+      res.status(400).json({ error: "Error, take must be a non-negative integer" })
+      return 
+    }
+  }
+
+  let result = posts;
+
+  if (skip !== undefined) {
+    result = result.slice(skip);
+  }
+
+  if (take !== undefined) {
+    result = result.slice(0, take);
+  }
+
+  res.json(result);
 });
 
-app.get('/posts/:id', (req, res) => {
-    const id = +req.params.id; 
-    if (isNaN(id)) {
-        res.status(400).json("Id must be an Integer")
-        return;
-    }
+app.get("/posts/:id", (req, res) => {
+  const id = +req.params.id;
+  if (isNaN(id)) {
+    res.status(400).json("Id must be an Integer");
+    return;
+  }
 
-    const post = posts.find((p) => { 
-        return p.id === id;
-    });
+  const post = posts.find((p) => p.id === id);
 
-    if (!post) {
-        res.status(404).json("Post not found")
-        return;
-    }
+  if (!post) {
+    res.status(404).json("Post not found");
+    return;
+  }
 
-
-    res.status(200).json(post);
+  res.status(200).json(post);
 });
-
-
-
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:8001`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
