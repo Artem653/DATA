d098631ccf70e54b754813b5ed57b63c2043f9fa
Artem653 <gromboom1325@gmail.com>
2025-10-03 17:33:19 +0300

    POST

--- server.js
+++ server.js
@@ -1,58 +1,85 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
-const moment = require("moment");
-
+const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+const path = require('path');
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+
+const HOST = "localhost";
 const PORT = 8001;
+const postsPath = path.join(__dirname, "posts.json");
 
-const productsPath = path.join(__dirname, "posts.json");
-const posts = JSON.parse(fs.readFileSync(productsPath, "utf-8"));
+const posts = JSON.parse(fs.readFileSync(postsPath, 'utf-8'));
 
-function getDate() {
-  return moment().format("YYYY/MM/DD HH:mm:ss");
-}
-
-app.get("/timestamp", (req, res) => {
-  res.json({ timestamp: getDate() });
+app.get('/', (req, res) => {
+    res.json('Hello world');
 });
 
-app.get("/posts", (req, res) => {
-  let { skip, take } = req.query;
+app.get('/posts', (req, res) => {
+    let { skip, take } = req.query;
 
-  skip = Number(skip);
-  take = Number(take);
+    skip = Number(skip);
+    take = Number(take);
 
-  let result = posts;
+    let result = posts;
 
-  if (!isNaN(skip) && skip > 0) {
-    result = result.slice(skip);
-  }
+    if (!isNaN(skip) && skip > 0) result = result.slice(skip);
+    if (!isNaN(take) && take > 0) result = result.slice(0, take);
 
-  if (!isNaN(take) && take > 0) {
-    result = result.slice(0, take);
-  }
-
-  res.json(result);
+    res.json(result);
 });
 
-app.get("/posts/:id", (req, res) => {
-  const id = +req.params.id;
-  if (isNaN(id)) {
-    res.status(400).json("Id must be an Integer");
-    return;
-  }
+app.get('/posts/:id', (req, res) => {
+    const id = +req.params.id;
+    if (isNaN(id)) {
+        res.status(400).json("Id must be an Integer");
+        return;
+    }
 
-  const post = posts.find((p) => p.id === id);
+    const post = posts.find(p => p.id === id);
+    if (!post) {
+        res.status(404).json("Post not found");
+        return;
+    }
 
-  if (!post) {
-    res.status(404).json("Post not found");
-    return;
-  }
-
-  res.status(200).json(post);
+    res.json(post);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:8001`);
+app.post('/posts', async (req, res) => {
+    const body = req.body;
+
+    if (!body) {
+        res.status(422).json('Body is required!');
+        return;
+    }
+
+    const newPost = { ...body, id: posts.length };
+
+    if (!newPost.title) {
+        res.status(422).json('Title is required!');
+        return;
+    }
+    if (!newPost.description) {
+        res.status(422).json('Description is required!');
+        return;
+    }
+    if (!newPost.image) {
+        res.status(422).json('Image is required!');
+        return;
+    }
+
+    try {
+        posts.push(newPost);
+        await fsPromises.writeFile(postsPath, JSON.stringify(posts, null, 4));
+        res.status(201).json(newPost);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json("Post creation failed");
+    }
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://localhost:8001`);
 });
